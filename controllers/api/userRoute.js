ccb73c15169009df8bd314f17b8af44028799322
Miles Thomas <milesman2013@gmail.com>
2022-11-20 19:16:47 +1100

    login route set up

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -1,20 +1,48 @@
 const router = require('express').Router();
-const { User } = require('../../models/user');
+const session = require('express-session');
+const { User } = require('../../models');
 
 router.post('/signup', async (req, res) => {
     try {
 
     } catch (err) {
+        console.log(err)
         res.status(500).json(err);
-    }
+    };
 });
 
 router.post('/login', async (req, res) => {
     try {
+        const userData = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
 
+        if (!userData) {
+            res
+            .status(400)
+            .json({ message: 'The login entered does not match a user in our database. Please try again or sign up to create a new account!' });
+            return;
+        }
+
+        const userPassword = await userData.checkPassword(req.body.password)
+
+        if (!userPassword) {
+            res
+            .status(400)
+            .json({ message: 'The login entered does not match a user in our database. Please try again or sign up to creaate a new account!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.logginIn = true
+
+            res.status(200).json({ user: userData, message: 'Welcome to QuizMe!'})
+        })
     } catch (err) {
         res.status(500).json(err);
-    }
+    };
 
 });
 
