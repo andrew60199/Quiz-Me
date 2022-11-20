b59f573723b8d3065c8b1b7b8d8cebb6c8842061
Miles Thomas <milesman2013@gmail.com>
2022-11-20 18:50:57 +1100

    security routes templated

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -1,5 +1,25 @@
 const router = require('express').Router();
 const { User } = require('../../models/user');
 
+router.post('/signup', async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+
+router.post('/logout', async (req, res) => {
+
+});
 
 module.exports = router;
