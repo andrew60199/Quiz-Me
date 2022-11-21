e96f85b9a93de2b27ef4418dced89997b0d7230d
AND.EVV <andrew60199@gmail.com>
2022-11-21 19:27:01 +1100

    added new models needed for the application

---
+++ models/quiz.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize')
+
+const sequelize = require('../config/connection')
+
+class Quiz extends Model {}
+
+Quiz.init({
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    question_one: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    question_two: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    question_three: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    question_four: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    correct_answer: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'quiz',
+  }
+);
+
+module.exports = Quiz
