da2eb50a29c1b8ac2af7987f0a65f7d7da4cf249
Miles Thomas <milesman2013@gmail.com>
2022-11-26 16:33:18 +1100

    potential solution for selecting correct answer

--- public/js/createquiz.js
+++ public/js/createquiz.js
@@ -1,3 +1,5 @@
+const possibleAnswers = document.querySelectorAll('input[name="correct-answer"]')
+
 async function newFormHandler(event) {
     event.preventDefault();
   
@@ -6,7 +8,40 @@ async function newFormHandler(event) {
     const answerTwo = document.querySelector('#answer-two').value;
     const answerThree = document.querySelector('#answer-three').value;
     const answerFour = document.querySelector('#answer-four').value;
-    const correctAnswer = ''
+    let correctAnswer
+
+    possibleAnswers.addEventListener('click', () => {
+      for (const selected of possibleAnswers) {
+        if (selected.checked) {
+          correctAnswer = selected.value
+        }
+      }
+    })
+
+
+
+    if (question && answerOne && answerTwo && answerThree && answerFour && correctAnswer) {
+      const response = await fetch('/api/quiz/upload', {
+        method: 'POST',
+        body: JSON.stringify({
+          question,
+          answerOne,
+          answerTwo,
+          answerThree,
+          answerFour,
+          correctAnswer
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+
+      if (response.ok) {
+        document.location.replace('/')
+      } else {
+        alert('Something went wrong, please try again')
+      }
+    }
 
     // Need to go through the form and see which button has been selected.
     // Then we can set the correctAnswer as the same value...  
